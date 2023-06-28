86308c2d4750af92b20b449ce3efcc0d35338690
Pratham Sharma <sharma.pratham0502@gmail.com>
2023-06-28 12:26:46 -0230

    Revert "added controllers and routes for config operations"
    This reverts commit 8e28e188d5ba01c76b70441bde17efedfd731d04.

--- editor-api/routes/configRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import * as configController from "../controllers/configController";
-
-const router = express.Router();
-
-router.get("/", configController.getConfig);
-router.put("/", configController.updateConfig);
-
-export default router;
